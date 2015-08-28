5addbf69a17689666be11e54c76e2b926e792d2e
sensei <sensei@test.com>
2015-08-28 21:14:07 +0900

    project init
    プロジェクト作成

---
+++ tourabutable/tourabutable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tourabutable
+//
+//  Created by sensei on 2015/08/28.
+//  Copyright (c) 2015年 sensei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
